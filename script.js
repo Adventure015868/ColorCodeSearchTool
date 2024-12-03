9cec6e14aa9a35e0ba0015975bb3720e393663a5
Adventure015868 <Tatsuya.tt.1127@gmail.com>
2024-12-03 17:29:03 +0900

    Site where you can search for color codes

---
+++ script.js
@@ -0,0 +1,24 @@
+const text=document.querySelector("#colorText");
+
+const color=document.querySelector("#colorPicker");
+
+//カラーピッカーを操作した時の一連の動作
+const colorBg=()=>{
+    //選択した色を背景色に設定
+    document.body.style.backgroundColor=color.value;
+
+    //カラーコードを表示
+    if(color.value==="#ffffff"){
+        //白色が選択されたとき
+        text.textContent="カラーコード:"+color.value(white);
+    } else if(color.value==="#000000"){
+        //黒色が選択されたとき
+        text.textContent="カラーコード:"+color.value(black);
+    } else{
+        //白色が選択されなかったとき
+        text.textContent="カラーコード:"+color.value;
+    }
+}
+
+//カラーピッカーが変更されたら関数colorBgを発動させる
+color.addEventListener("input",colorBg);
